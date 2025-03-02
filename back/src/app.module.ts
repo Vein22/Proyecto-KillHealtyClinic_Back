9543cb633f9b0331f6528493d1101dbb10953a84
MMelvinMora <121642949+Vein22@users.noreply.github.com>
2025-03-02 16:30:40 -0400

    Initial Back Configuration

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config'
+import { ConfigService } from '@nestjs/config/dist/config.service';
+import { postgresDataSourceConfig } from './config/data-source';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [postgresDataSourceConfig]
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({...configService.get('data-source')})
+    }),
+    UsersModule, AuthModule],
   controllers: [],
   providers: [],
 })
---
+++ back/src/config/data-source.ts
@@ -0,0 +1,25 @@
+import { DataSourceOptions } from "typeorm";
+import { config as dotenvConfig } from "dotenv"
+import { registerAs } from "@nestjs/config";
+
+dotenvConfig({
+    path: ".env"
+});
+
+const postgresDataSourceOptions: DataSourceOptions = {
+    type: "postgres",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : undefined,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    schema: 'public',
+    dropSchema: false,
+    synchronize: true,
+    logging: true,
+    entities: ["dist/**/*.entity{.ts,.js}"],
+    migrations: ["dist/migrations/*{.js,.ts}"],
+    ssl: false,
+}
+
+export const postgresDataSourceConfig = registerAs("data-source", () => postgresDataSourceOptions)
