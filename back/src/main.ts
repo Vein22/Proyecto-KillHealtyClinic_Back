b7a0862cff996c2a5328b8b62eda7fc5076ce486
MMelvinMora <121642949+Vein22@users.noreply.github.com>
2025-03-04 21:17:28 -0400

    cors

--- back/src/main.ts
+++ back/src/main.ts
@@ -3,8 +3,9 @@ import { AppModule } from './app.module';
 import { LoggerGlobal } from './middleware/loger.middleware';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true }); 
   app.use(LoggerGlobal);
+  app.enableCors({ origin: '*', methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', credentials: true, });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
