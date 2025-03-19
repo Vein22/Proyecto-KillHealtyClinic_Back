0cb455982dcc20796231d0f6b255c0b9670447f9
MMelvinMora <121642949+Vein22@users.noreply.github.com>
2025-03-18 23:19:17 -0400

    Get all users route, protected by roles

--- back/src/users/users.controller.ts
+++ back/src/users/users.controller.ts
@@ -1,6 +1,9 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { UseGuards } from '@nestjs/common/decorators';
+import { Roles } from 'src/decorators/roles.decorator';
 import { AuthGuard } from 'src/guards/auth.guard';
+import { RolesGuard } from 'src/guards/roles.guard';
+import { Role } from 'src/roles/roles.enum';
 import { UsersService } from './users.service';
 
 @Controller('users')
@@ -9,7 +12,8 @@ export class UsersController {
 
 
   @Get()
-  @UseGuards(AuthGuard)
+  @Roles(Role.Admin, Role.Doctor)
+  @UseGuards(AuthGuard, RolesGuard)
   async findAll(@Query('page') page: number=1, @Query('limit') limit: number=5) {
     return this.usersService.findAll(page, limit);
   }
